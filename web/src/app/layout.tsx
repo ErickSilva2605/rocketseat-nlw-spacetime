f7b47e5df89ee28c5923212522b56be2652564a0
ErickSilva2605 <erick.dev.it@gmail.com>
2023-08-13 22:37:56 -0300

    [Web] - Criando a rota de logout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 import { cookies } from 'next/headers'
 
 import { Copyright } from '@/components/Copyright'
-import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
 import { Profile } from '@/components/Profile'
